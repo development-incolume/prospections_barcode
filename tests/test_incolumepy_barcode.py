c7eca02b14978588d77113b3222ee34cbb834048
Britodfbr <britodfbr@gmail.com>
2023-01-22 11:53:04 -0300

    feat: Unificação de versionamento

--- tests/test_incolumepy_barcode.py
+++ tests/test_incolumepy_barcode.py
@@ -1,5 +1,17 @@
 from incolumepy.barcode import __version__
+import re
+import pytest
 
 
-def test_version():
-    assert __version__ == '0.1.0-a0'
+@pytest.mark.parametrize(
+    'entrance expected'.split(),
+    (
+        ('0.1.0', True),
+        ('10.11.10', True),
+        ('1.11.10-alpha.999', True),
+        ('1.1.1-a.9', True),
+        ('1.1.1a9', True),
+    )
+)
+def test_version(entrance, expected):
+    assert bool(re.fullmatch(r'(\d+\.){2}\d+(-?\w+\.?\d+)?', entrance, flags=re.I)) == expected
